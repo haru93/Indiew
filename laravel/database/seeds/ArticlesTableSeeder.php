33f58b3bf86ecbe56bdfa7f0437cfa0233c7002d
haru93 <syuesuta@yahoo.co.jp>
2020-08-18 15:03:05 +0900

    記事表示機能一式を追加

---
+++ laravel/database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('articles')->insert([
+            'user_id' => 1,
+            'title' => 'タイトルテスト',
+            'body' => '内容テスト',
+            'image' => '###',
+        ]);
+
+        DB::table('articles')->insert([
+            'user_id' => 1,
+            'title' => 'タイトルテスト2',
+            'body' => '内容テスト2',
+            'image' => '######',
+        ]);
+    }
+}
