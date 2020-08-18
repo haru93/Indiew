6cbc2d305fe8ddb4a66b0f594533d37cfeca8237
haru93 <syuesuta@yahoo.co.jp>
2020-08-18 17:09:03 +0900

    画像投稿機能を追加

--- laravel/resources/views/articles/index.blade.php
+++ laravel/resources/views/articles/index.blade.php
@@ -4,18 +4,25 @@
     <div class="container">
         <div class="row justify-content-center">
             @foreach ($articles as $article)
-            <div class="col-md-4 mb-2">
-                <div class="card">
-                    <div class="card-header">
-                        {{ $article->title }}
-                    </div>
-                    <div class="card-body">
-						<h5 class="card-title">{{ $article->body }}</h5>
-                        <p class="card-text">{{ $article->image }}</p>
+                <div class="col-md-4 mb-2">
+                    <div class="card">
+                        <div class="card-header">
+                            {{ $article->title }}
+                        </div>
+                        <div class="card-body">
+                            @if(!empty($article->image))
+                                <div class='image-wrapper'><img class='view-image' src="{{ asset('storage/images/'.$article->image) }}"></div>
+                            @else
+                                <div class='image-wrapper'><img class='view-image' src="{{ asset('images/dummy.png') }}"></div>
+                            @endif
+                            <p class="card-text">{{ $article->body }}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
             @endforeach
+        </div>
+        <div class="row justify-content-center">
+            {{ $articles->links() }}
         </div>
     </div>
 @endsection
