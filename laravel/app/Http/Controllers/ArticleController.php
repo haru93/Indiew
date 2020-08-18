66886e50606897b86257d62016d3bcd93d93c130
haru93 <syuesuta@yahoo.co.jp>
2020-08-18 18:23:01 +0900

    フォームリクエストバリデーションを使った形に変更

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
+use App\Http\Requests\StoreArticleForm;
 
 class ArticleController extends Controller
 {
@@ -35,15 +36,9 @@ class ArticleController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreArticleForm $request)
     {
         $post = $request->all();
-
-        $validatedData = $request->validate([
-            'title' => 'required|max:255',
-            'body' => 'required',
-            'image' => 'mimes:jpeg,png,jpg,gif,svg|max:2048',
-        ]);
         
         if ($request->hasFile('image')) {
             $request->file('image')->store('/public/images');
