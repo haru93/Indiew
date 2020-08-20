d4f934f9559836278ddb2b30968c708827de6b45
haru93 <syuesuta@yahoo.co.jp>
2020-08-20 18:42:37 +0900

    OGPの追加

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
+<head prefix="og: http://ogp.me/ns# website: http://ogp.me/ns/website#">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -9,12 +9,24 @@
 
     <title>{{ config('app.name', 'TripSwitch') }}</title>
 
+    <!-- OGP -->
+    <meta name="description"         content="NintendoSwitchで手に入るインディーズゲームの景色を投稿するサイト">
+    <meta property="og:title"        content="NintendoSwitchで旅を">
+    <meta property="og:description"  content="NintendoSwitchで手に入るインディーズゲームの景色を投稿するサイト">
+    <meta property="og:type"         content="website">
+    <meta property="og:site_name"    content="TripSwitch">
+    <meta property="og:url"          content="https://trip-switch.herokuapp.com">
+    <meta property="og:image"        content="{{ secure_asset('images/opg.jpeg') }}">
+    <meta property="og:image:width"  content="1200">
+    <meta property="og:image:height" content="630">
+    <meta name="twitter:card"        content="summary_large_image">
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- 本番環境用 -->
     @if(app('env') == 'production')
+    <!-- Production -->
         <!-- favicon -->
         <link rel="shortcut icon" href="{{ secure_asset('images/logo.png') }}">
         <!-- Scripts -->
@@ -22,9 +34,8 @@
         <!-- Styles -->
         <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ secure_asset('css/layout.css') }}" rel="stylesheet">
-        
-    <!-- ローカル環境用 -->
     @else
+    <!-- Local -->
         <!-- favicon -->
         <link rel="shortcut icon" href="{{ asset('images/logo.png') }}">
         <!-- Scripts -->
