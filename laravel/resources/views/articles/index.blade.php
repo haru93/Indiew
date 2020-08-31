5d48b7384c803ec06533ed2fac86b1ffa5b527cc
haru93 <syuesuta@yahoo.co.jp>
2020-09-01 00:07:47 +0900

    TopページのViewの修正

--- laravel/resources/views/articles/index.blade.php
+++ laravel/resources/views/articles/index.blade.php
@@ -6,25 +6,27 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            @foreach ($articles as $article)
-                <div class="col-md-8 mb30">
-                    <a href="{{ route('articles.show', compact('article')) }}" class="title-link">
-                        <div class='image-wrapper-index'>
-                            @if(app('env') == 'production')
-                                <img class='image-box' src="{{ $article->image }}">
-                            @else
-                                <img class='image-box' src="{{ asset('storage/'.$article->image) }}">
-                            @endif
-                                <div class="mask">
-                                    <div class="caption-title">{{ $article->title }}</div>
-                                </div>
-                                <div class="caption-gamename">{{ $article->game->name }}</div>
-                        </div>
-                    </a>
-                </div>
-            @endforeach
-        </div>
+        @foreach ($articles->chunk(2) as $chunk)
+            <div class="row justify-content-center index-2col">
+                @foreach ($chunk as $article)
+                    <div class="col-md-5 mb30">
+                        <a href="{{ route('articles.show', compact('article')) }}" class="title-link">
+                            <div class='image-wrapper-index'>
+                                @if(app('env') == 'production')
+                                    <img class='image-box' src="{{ $article->image }}">
+                                @else
+                                    <img class='image-box' src="{{ asset('storage/'.$article->image) }}">
+                                @endif
+                                    <div class="mask">
+                                        <div class="caption-title">{{ $article->title }}</div>
+                                    </div>
+                                    <div class="caption-gamename">{{ $article->game->name }}</div>
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
         <div class="row justify-content-center">
             {{ $articles->links() }}
         </div>
