3929cb7597a4a132fe2692448f267398b72f8405
haru93 <syuesuta@yahoo.co.jp>
2020-09-01 00:18:56 +0900

    cssのリファクタリング

--- laravel/resources/views/articles/index.blade.php
+++ laravel/resources/views/articles/index.blade.php
@@ -13,9 +13,9 @@
                         <a href="{{ route('articles.show', compact('article')) }}" class="title-link">
                             <div class='image-wrapper-index'>
                                 @if(app('env') == 'production')
-                                    <img class='image-box' src="{{ $article->image }}">
+                                    <img src="{{ $article->image }}">
                                 @else
-                                    <img class='image-box' src="{{ asset('storage/'.$article->image) }}">
+                                    <img src="{{ asset('storage/'.$article->image) }}">
                                 @endif
                                     <div class="mask">
                                         <div class="caption-title">{{ $article->title }}</div>
