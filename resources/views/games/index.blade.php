76c916d7da95d729c00ff28221b6f7b6ecbb36ed
haru93 <syuesuta@yahoo.co.jp>
2020-10-11 07:06:32 +0900

    ゲーム一覧にサイドバーを追加

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -3,27 +3,85 @@
 @section('content')
     <div class="container">
         <h2 class='mt-3 mb-2'>ゲーム一覧</h2>
-        @foreach ($games as $game)
-        <div class="border-top"></div>
-        <div class="row mt-4 mb-4 position-relative">
-            <div class="col-lg-4">
-                <div class="games-search-image">
-                    @if(app('env') == 'production')
-                        <img class='card-img-top' src="{{ $game->image }}">
-                    @else
-                        <img class='card-img-top' src="{{ asset('storage/'.$game->image) }}">
-                    @endif
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="border-top"></div>
+                <div class="accordion games-index-sidebar" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                            <h5 class="mb-0">
+                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Collapsible Group Item #1
+                            </button>
+                            </h5>
+                        </div>
+
+                        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div class="card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="border-bottom"></div>
+
+                    <div class="card">
+                        <div class="card-header" id="headingTwo">
+                            <h5 class="mb-0">
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Collapsible Group Item #2
+                            </button>
+                            </h5>
+                        </div>
+                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                            <div class="card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="border-bottom"></div>
+
+                    <div class="card">
+                        <div class="card-header" id="headingThree">
+                            <h5 class="mb-0">
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                Collapsible Group Item #3
+                            </button>
+                            </h5>
+                        </div>
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                            <div class="card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="border-bottom"></div>
                 </div>
             </div>
-            <div class="col-lg-6 games-search-data">
-                <h3>{{ $game->name }}</h3>
-                <p>{{ $game->data }}</p>
+
+            <div class="col-lg-9">
+                @foreach ($games as $game)
+                <div class="border-top"></div>
+                <div class="row mt-4 mb-4 position-relative">
+                    <div class="col-lg-4">
+                        <div class="games-search-image">
+                            @if(app('env') == 'production')
+                                <img class='card-img-top' src="{{ $game->image }}">
+                            @else
+                                <img class='card-img-top' src="{{ asset('storage/'.$game->image) }}">
+                            @endif
+                        </div>
+                    </div>
+                    <div class="col-lg-6 games-search-data">
+                        <h4>{{ $game->name }}</h4>
+                        <p>{{ $game->data }}</p>
+                    </div>
+                    <div class="col-lg-2 text-right">
+                        <h6>{{ number_format($game->price) }}円(税込)</h6>
+                    </div>
+                    <a href="{{ route('games.show', ['id' => $game->id]) }}" class="stretched-link"></a>
+                </div>
+                @endforeach
             </div>
-            <div class="col-lg-2 text-right">
-                <h5>{{ number_format($game->price) }}円(税込)</h5>
-            </div>
-            <a href="{{ route('games.show', ['id' => $game->id]) }}" class="stretched-link"></a>
         </div>
-        @endforeach
     </div>
 @endsection
