e3ff3ebf655ee965a673c5b9452a9313e544486b
haru93 <syuesuta@yahoo.co.jp>
2020-10-11 05:40:24 +0900

    ゲーム一覧ページのViewを改良

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -3,14 +3,27 @@
 @section('content')
     <div class="container">
         <h2 class='mt-3 mb-2'>ゲーム一覧</h2>
-        <div class="row justify-content-center mb-3">
-            <div class="col-md-8">
-                <ul>
-                    @foreach ($games as $game)
-                        <li><a href="{{ route('games.show', ['id' => $game->id]) }}">{{ $game->name }}</a></li>
-                    @endforeach
-                </ul>
+        @foreach ($games as $game)
+        <div class="border-top"></div>
+        <div class="row mt-4 mb-4 position-relative">
+            <div class="col-lg-4">
+                <div class="games-search-image">
+                    @if(app('env') == 'production')
+                        <img class='card-img-top' src="{{ $game->image }}">
+                    @else
+                        <img class='card-img-top' src="{{ asset('storage/'.$game->image) }}">
+                    @endif
+                </div>
             </div>
+            <div class="col-lg-6 games-search-data">
+                <h3>{{ $game->name }}</h3>
+                <p>{{ $game->data }}</p>
+            </div>
+            <div class="col-lg-2 text-right">
+                <h5>{{ number_format($game->price) }}円(税込)</h5>
+            </div>
+            <a href="{{ route('games.show', ['id' => $game->id]) }}" class="stretched-link"></a>
         </div>
+        @endforeach
     </div>
 @endsection
