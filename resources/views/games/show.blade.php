633ce1f0746ac1c82ed74e99cf2fa015929d4b95
haru93 <syuesuta@yahoo.co.jp>
2020-10-15 05:51:24 +0900

    どの画像に関わらず高さを調整し表示されるようCSS修正 close #41

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -1,73 +1,72 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h2 class='mt-3 mb-2'>ゲーム紹介</h2>
-        <div class="row justify-content-center mb-3">
-            <div class="col">
-                <div class="card games-show-image">
-                    @if(app('env') == 'production')
-                        <img class='card-img-top' src="{{ $game->image }}">
-                    @else
-                        <img class='card-img-top' src="{{ asset('storage/'.$game->image) }}">
-                    @endif
-                    <div class="card-body">
-                        <h3 class="card-title">{{ $game->name }}</h3>
-                        <p class="card-text">{{ $game->data }}</p>
-                        <div class="row">
-                            <div class="col-md-9 mt-3">
-                                <table class="table">
-                                    <tbody>
-                                        <tr>
-                                            <th>販売価格</th>
-                                            <td>{{ number_format($game->price) }}円(税込)</td>
-                                        </tr>
-                                        <tr>
-                                            <th>カテゴリー</th>
-                                            <td>{{ $game->category->name }}</td>
-                                        </tr>
-                                        <tr>
-                                            <th>配信日</th>
-                                            <td>{{ $game->released_date->format('Y年m月d日') }}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+<div class="container">
+    <div class="row justify-content-center mt-5 mb-3">
+        <div class="col-lg-8 games-show-card">
+            <div class="card">
+                @if(app('env') == 'production')
+                <img class='card-img-top w-100' src="{{ $game->image }}">
+                @else
+                <img class='card-img-top w-100' src="{{ asset('storage/'.$game->image) }}">
+                @endif
+                <div class="card-body">
+                    <h3 class="card-title">{{ $game->name }}</h3>
+                    <p class="card-text">{{ $game->data }}</p>
+                    <div class="row">
+                        <div class="col-md-9 mt-3">
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <th>販売価格</th>
+                                        <td>{{ number_format($game->price) }}円(税込)</td>
+                                    </tr>
+                                    <tr>
+                                        <th>カテゴリー</th>
+                                        <td>{{ $game->category->name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>配信日</th>
+                                        <td>{{ $game->released_date->format('Y年m月d日') }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="col-md-3 mt-3">
+                            <div class="text-center mb-3">
+                                <a class="btn btn-success w-100" href="{{ route('articles.preCreate', ['id' => $game->id]) }}">
+                                投稿する</a>
                             </div>
-                            <div class="col-md-3 mt-3">
-                                <div class="text-center mb-3">
-                                    <a class="btn btn-success w-100" href="{{ route('articles.preCreate', ['id' => $game->id]) }}">
-                                    この作品の景色を投稿する</a>
-                                </div>
-                                <div class="text-center">
-                                    <a class="btn btn-danger w-100" href="{{ $game->url }}" target="_blank">My Nintendo Store</a>
-                                </div>
+                            <div class="text-center">
+                                <a class="btn btn-danger w-100" href="{{ $game->url }}" target="_blank">販売ページ</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <h4 class='mt-3 mb-3'>関連記事</h4>
-        <div class="mb-3">
+    <h4 class='my-4'>関連記事</h4>
+    <div class="mb-3">
         <div class="row justify-content-left image-col pl-3 pl-3">
             @foreach ($game->articles as $article)
-                <div class="col-md-4">
-                    <a href="{{ route('articles.show', compact('article')) }}" class="title-link">
-                        <div class='image-wrapper'>
-                            @if(app('env') == 'production')
-                                <img src="{{ $article->image }}">
-                            @else
-                                <img src="{{ asset('storage/'.$article->image) }}">
-                            @endif
-                            <div class="mask">
-                                <div class="caption-title">{{ $article->title }}</div>
-                            </div>
+            <div class="col-md-4">
+                <a href="{{ route('articles.show', compact('article')) }}">
+                    <div class='image-wrapper'>
+                        @if(app('env') == 'production')
+                        <img src="{{ $article->image }}" height="200">
+                        @else
+                        <img src="{{ asset('storage/'.$article->image) }}" height="200">
+                        @endif
+                        <div class="mask">
+                            <div class="caption-title">{{ $article->title }}</div>
                         </div>
-                    </a>
-                </div>
+                    </div>
+                </a>
+            </div>
             @endforeach
         </div>
-        </div>
     </div>
+</div>
 @endsection
