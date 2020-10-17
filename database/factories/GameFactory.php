5a5c0f9d516843fedf34683c7ae1166a156f534c
haru93 <syuesuta@yahoo.co.jp>
2020-10-18 03:59:18 +0900

    テーブル設計の変更によるテストコード(Factory)の修正

--- database/factories/GameFactory.php
+++ database/factories/GameFactory.php
@@ -13,9 +13,6 @@ $factory->define(Game::class, function (Faker $faker) {
         'image' => $faker->image,
         'url' => $faker->url,
         'price' => $faker->numberBetween($min = 100, $max = 200),
-        'category_id' => function () {
-            return factory(Category::class);
-        },
         'released_date' =>$faker->date('Y-m-d', 'now')
     ];
 });
