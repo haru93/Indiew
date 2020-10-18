0cb16707389793b34961b6b9cfd02efaf058c09a
haru93 <syuesuta@yahoo.co.jp>
2020-10-18 18:13:13 +0900

    ゲームの登録数を増やす close #67

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -11,32 +11,44 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
+        // 1
         DB::table('categories')->insert([
             'name' => 'シミュレーション',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
 
+        // 2
         DB::table('categories')->insert([
             'name' => 'アドベンチャー',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
 
+        // 3
         DB::table('categories')->insert([
             'name' => 'アクション',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
 
+        // 4
         DB::table('categories')->insert([
-            'name' => 'RPG',
+            'name' => 'レース',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
 
+        // 5
         DB::table('categories')->insert([
             'name' => 'シューティング',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        // 6
+        DB::table('categories')->insert([
+            'name' => 'スポーツ',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
--- database/seeds/CategoryGameTableSeeder.php
+++ database/seeds/CategoryGameTableSeeder.php
@@ -45,5 +45,68 @@ class CategoryGameTableSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now(),
         ]);
+
+        DB::table('category_game')->insert([
+            'category_id' => 2,
+            'game_id' => 5,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('category_game')->insert([
+            'category_id' => 2,
+            'game_id' => 6,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('category_game')->insert([
+            'category_id' => 3,
+            'game_id' => 6,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('category_game')->insert([
+            'category_id' => 2,
+            'game_id' => 7,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('category_game')->insert([
+            'category_id' => 3,
+            'game_id' => 7,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('category_game')->insert([
+            'category_id' => 3,
+            'game_id' => 8,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('category_game')->insert([
+            'category_id' => 4,
+            'game_id' => 9,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('category_game')->insert([
+            'category_id' => 6,
+            'game_id' => 10,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('category_game')->insert([
+            'category_id' => 2,
+            'game_id' => 11,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
