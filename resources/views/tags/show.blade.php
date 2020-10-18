9b4b4ee9e74b7102669d8a4871091cfdb172f638
haru93 <syuesuta@yahoo.co.jp>
2020-10-19 03:36:08 +0900

    タグ検索後のView(tags.show)の修正 Fixes #73

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -2,14 +2,13 @@
 
 @section('content')
 	<div class="container">
-		<h2 class='mt-3 mb-2'>タグ</h2>
-		<div class="row justify-content-center mb-3">
-			<div class="col-md-8">
+		<div class="row justify-content-center mb-3 mt-5">
+			<div class="col-md-6 tags-show-card">
 				<div class="card">
 					<div class="card-body">
 						<h2 class="h4 card-title m-0">{{ $tag->name }}</h2>
 						<div class="card-text text-right">
-							{{ $tag->articles->count() }}件
+							{{ $tag->articles->count() }} 件の結果
 						</div>
 					</div>
 				</div>
@@ -22,7 +21,7 @@
 				<div class="col-md-4">
 					<a href="{{ route('articles.show', compact('article')) }}" class="title-link">
 						<div class='image-wrapper'>
-							<img src="{{ $article->image }}">
+							<img src="{{ $article->image }}" height="200">
 							<div class="mask">
 								<div class="caption-title">{{ $article->title }}</div>
 							</div>
